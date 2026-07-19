2162b172bcd575d167a176cc9b8c69669e582a28
Adam Nativ <admlvntv@gmail.com>
2026-07-19 16:14:06 -0400

    fix: add out of bounds check for fetch

--- src/CPU.cpp
+++ src/CPU.cpp
@@ -1,9 +1,14 @@
 #include "CPU.h"
 
 uint16_t CPU::fetch() {
+  // Out of bounds checking
+  if (m_pc >= MEMORY_SIZE - 1) {
+    throw std::out_of_range("PC out of bounds");
+  }
+
   // Combine current and next byte
-  uint8_t high_byte{ m_memory.at(m_pc) };
-  uint8_t low_byte{ m_memory.at(m_pc+1) };
+  uint8_t high_byte{ m_memory[m_pc] };
+  uint8_t low_byte{ m_memory[m_pc+1] };
   uint16_t opcode = { static_cast<uint16_t>((high_byte << 8) | low_byte) };
   m_pc += 2;
   return opcode;
