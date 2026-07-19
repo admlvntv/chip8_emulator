ea6598a32b5df3d016d36a4eeec97b268f360fc7
Adam Nativ <admlvntv@gmail.com>
2026-07-19 13:42:12 -0400

    feat: implement cpu fetch

--- src/CPU.cpp
+++ src/CPU.cpp
@@ -1,1 +1,8 @@
 #include "CPU.h"
+
+uint16_t CPU::fetch() {
+  // Combine current and next byte
+  uint16_t opcode = (m_memory.at(m_pc) << 8) | (m_memory.at(m_pc+1));
+  m_pc += 2;
+  return opcode;
+}
